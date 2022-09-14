cd6f549d7c46136b5b1c3631c29ce643fa1216c4
丹墨 <yixideximo@outlook.com>
2022-09-15 00:24:14 +0800

    Config File Type To Implement .Net Remoting

---
+++ RemotingDemo01/RemotingClient2/Program.cs
@@ -0,0 +1,31 @@
+﻿using RemotingModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemotingClient2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RemotingConfiguration.Configure("RemotingClient2.exe.config", false);
+            
+            var proxy = new TestRemotingObject();
+            if(proxy == null)
+            {
+                Console.WriteLine("Connecting to the server failed!");
+                return;
+            }
+
+            var channelType = new Uri(ChannelServices.GetUrlsForObject(proxy).First()).Scheme;
+            Console.WriteLine($"This call object by {channelType} Channel, {proxy.TestForChannel(channelType)}");
+            Console.WriteLine("Press Any Key To Exit!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RemotingDemo01/RemotingServer2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemotingServer2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RemotingConfiguration.Configure("RemotingServer2.exe.config", false);
+            foreach(var channel in ChannelServices.RegisteredChannels)
+            {
+                Console.WriteLine($"The name of the channel is {channel.ChannelName}");
+                Console.WriteLine($"The priority of the channel is {channel.ChannelPriority}");
+            }
+            Console.WriteLine("Press Any Key To Exit!");
+            Console.ReadKey();
+        }
+    }
+}
